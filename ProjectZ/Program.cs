441f630af4228770235664c0015744f4c1722bc3
LioV21 <villanuevalionel52104@gmail.com>
2023-03-06 22:50:15 -0800

    git commit

--- ProjectZ/Program.cs
+++ ProjectZ/Program.cs
@@ -1,8 +1,4 @@
-﻿// Health App 
-// Author: Lionel Villanueva
-// Purpose: This application allows a new user to create a profile by inputting personal information and completing a questionnaire. The application then displays a summary of the user's profile.
-
-namespace HealthApp
+﻿namespace HealthApp
 {
     class Program
     {
@@ -45,48 +41,38 @@ namespace HealthApp
             List<string> responses = new List<string>();
             Console.WriteLine("Please answer the following questions:");
 
-            Console.Write("1)Do you run daily? ");
-            responses.Add(Console.ReadLine());
+            string[] questions = new string[]
+            {
+                "Do you run daily?",
+                "Do you go to the gym daily?",
+                "How many push ups are you able to do?",
+                "How much can you lift?",
+                "Do you eat a balanced diet?",
+                "How many hours of sleep do you get per night?",
+                "How many glasses of water do you drink per day?",
+                "Do you smoke or use tobacco products?",
+                "Do you drink alcohol?",
+                "Do you have any medical conditions we should be aware of?"
+            };
 
-            Console.Write("2)Do you go to the gym daily? ");
-            responses.Add(Console.ReadLine());
-
-            Console.Write("3)How many push ups are you able to do? ");
-            responses.Add(Console.ReadLine());
-
-            Console.Write("4)How much can you lift? ");
-            responses.Add(Console.ReadLine());
-
-            Console.Write("5)Do you eat a balanced diet? ");
-            responses.Add(Console.ReadLine());
-
-            Console.Write("6)How many hours of sleep do you get per night? ");
-            responses.Add(Console.ReadLine());
-
-            Console.Write("7)How many glasses of water do you drink per day? ");
-            responses.Add(Console.ReadLine());
-
-            Console.Write("8)Do you smoke or use tobacco products? ");
-            responses.Add(Console.ReadLine());
-
-            Console.Write("9)Do you drink alcohol? ");
-            responses.Add(Console.ReadLine());
-
-            Console.Write("10)Do you have any medical conditions we should be aware of? ");
-            responses.Add(Console.ReadLine());
+            for (int i = 0; i < questions.Length; i++)
+            {
+                Console.Write($"Question {i + 1}: {questions[i]} ");
+                responses.Add(Console.ReadLine());
+            }
 
             // Display user profile summary
-            Console.WriteLine();
-            Console.WriteLine("===== User Profile Summary =====");
             Console.WriteLine($"Name: {lastName}, {firstName}");
             Console.WriteLine($"Age: {DateTime.Now.Year - birthYear}");
             Console.WriteLine($"Gender: {GetGenderDescription(gender)}");
-            Console.WriteLine();
 
-            Console.WriteLine("===== Questionnaire Responses =====");
+            Console.WriteLine("************ Your Responses ************");
+
             for (int i = 0; i < responses.Count; i++)
             {
-                Console.WriteLine($"Question {i + 1}: {responses[i]}");
+                Console.WriteLine($"Question {i + 1}: {questions[i]}");
+                Console.WriteLine($"Response {i + 1}: {responses[i]}");
+                Console.WriteLine("****************************************");
             }
         }
 
@@ -96,7 +82,6 @@ namespace HealthApp
         /// </summary>
         /// <param name="gender">The user's gender character (M/F/O).</param>
         /// <returns>The full description of the user's gender (Male/Female/Other not listed).</returns>
-        
         private static string GetGenderDescription(char gender)
         {
             switch (gender)
